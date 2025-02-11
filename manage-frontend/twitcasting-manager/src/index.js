7c8bff5db391e21dc1ce82ed4b8aaf234dc1333e
miutaku <miutaku@miutakuMacBook-Pro.local>
2025-02-11 21:37:06 +0900

    ReactDOMのインポート方法を更新し、サービスワーカーの登録コードを整理

--- manage-frontend/twitcasting-manager/src/index.js
+++ manage-frontend/twitcasting-manager/src/index.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// Register service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').then((registration) => {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, (err) => {
-      console.log('ServiceWorker registration failed: ', err);
-    });
+    navigator.serviceWorker.register('/sw.js').then(
+      (registration) => {
+        console.log('ServiceWorker registration successful with scope:', registration.scope);
+      },
+      (err) => {
+        console.log('ServiceWorker registration failed:', err);
+      }
+    );
   });
 }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
